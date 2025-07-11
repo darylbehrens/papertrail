deefad9e23b1ffba529afa85c1a6892b661ee382
vagrant <vagrant@vvv>
2025-07-11 19:13:39 +0000

    Working on Image issue

--- public/wp-content/themes/papertrail-theme/single-owl_sighting.php
+++ public/wp-content/themes/papertrail-theme/single-owl_sighting.php
@@ -2,10 +2,15 @@
 $context = Timber::context();
 $post = Timber::get_post();
 
+if ($post->thumbnail) {
+    $post->thumbnail = Timber::get_image($post->thumbnail->ID);
+} else {
+   $post->thumbnail = Timber::get_image('2025\07\Owl-Dead-on-SharpenAI-Focus-150x150.jpg');
+}
+
+
+
 $context['post'] = $post;
 $context['post_content'] = apply_filters('the_content', $post->post_content);
-
-// Optional: If you ever want to show related sightings or more metadata
-// $context['related_sightings'] = Timber::get_posts([...]);
 
 Timber::render(['single-owl_sighting.twig', 'single.twig'], $context);
